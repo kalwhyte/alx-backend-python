5ef9bcc7054c5b5de61537b5fd5c3af35c3e68b4
whytewalker <whytewalkerinstalltion@gmail.com>
2023-08-04 00:58:20 +0100

    A type-annotated function add that takes a float a and a float b as arguments and return their sum as a float A typr-annotated function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module for Basic annotations - add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Returns the sum of two floats.
+
+    Args:
+        a (float): First float.
+        b (float): Second float.
+
+    Returns:
+        float: The sum of the two floats.
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module for Basic annotations - concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Returns the concatenation of two strings.
+
+    Args:
+        str1 (str): First string.
+        str2 (str): Second string.
+
+    Returns:
+        str: The concatenation of the two strings.
+    """
+    return str1 + str2
