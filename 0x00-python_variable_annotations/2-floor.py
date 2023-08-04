a4e8dfe29fa309b2869beefc4d057c9526cb1b7a
whytewalker <whytewalkerinstalltion@gmail.com>
2023-08-04 01:05:17 +0100

    floating-point int

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module contains the function floor which takes a float n as argument
+and returns the floor of the float.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    This function takes a float n as argument and returns the floor of the float.
+    """
+    return int(n)
