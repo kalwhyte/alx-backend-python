cf8feb1e5056ab0c829b03edbb311498d963f482
whytewalker <whytewalkerinstalltion@gmail.com>
2023-08-04 01:37:11 +0100

    to string

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -6,11 +6,5 @@
 def to_str(n: float) -> str:
     """
     Return the string representation of the float.
-
-    Args:
-        n (float): The float to convert to a string.
-
-    Returns:
-        str: The string representation of the float.
     """
     return str(n)
