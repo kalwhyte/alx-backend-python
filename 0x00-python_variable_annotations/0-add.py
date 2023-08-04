a83793738caa333ca53aec3344b139321574063b
whytewalker <whytewalkerinstalltion@gmail.com>
2023-08-04 02:37:52 +0100

    annotation of float arguements

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,5 +5,12 @@
 
 def add(a: float, b: float) -> float:
     '''Returns the sum of two floats.
+
+    Args:
+        a (float): first float
+        b (float): second float
+
+    Returns:
+        float: sum of a and b
     '''
     return float(a + b)
