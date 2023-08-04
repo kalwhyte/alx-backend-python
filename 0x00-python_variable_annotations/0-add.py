97d419c3e251e5905629640b8cfda3924226baf7
Ejiofor Emmanuel Arinze <64577461+kalwhyte@users.noreply.github.com>
2023-08-04 04:35:45 +0100

    Update 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -7,6 +7,12 @@ Module for basic annotations - add
 def add(a: float, b: float) -> float:
     """
     Adds two float numbers.
+
+    Args:
+        a (float): First number.
+        b (float): Second number.
+
+    Returns:
+        float: The sum of two floats
     """
     return a + b 
-
