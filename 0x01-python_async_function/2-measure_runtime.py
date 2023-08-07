e80c07ee94903aeeaa6fe0c422a749d6eeeb70a0
whytewalker <whytewalkerinstalltion@gmail.com>
2023-08-07 17:34:12 +0100

    creating a measure_time function with integers n and max_delay

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-""" 
+''' 
     Using the (async) measure_time function
-"""
+'''
 import asyncio
 import time
 
@@ -10,10 +10,8 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """ 
-        Measure the total execution time for wait_n(n, max_delay), and
-        return the total_time / n. Your function should return a float.
-    """
+    '''Measure the total execution time for wait_n(n, max_delay)
+    '''
     start = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
     return (time.perf_counter() - start) / n
