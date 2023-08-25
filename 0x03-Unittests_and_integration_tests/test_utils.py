dfd5d32c0622c3d271bf504a22d1ff468408bc84
kalwhyte <superkalel55@gmail.com>
2023-08-25 20:20:03 +0100

    Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -72,3 +72,7 @@ class TestMemoize(unittest.TestCase):
             test_class.a_property()
             test_class.a_property()
             mock.assert_called_once()
+
+
+if __name__ == '__main__':
+    unittest.main()
