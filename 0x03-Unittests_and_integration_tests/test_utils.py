2ae4bf71897b6a4795f0a39a5ca4243502c1442e
kalwhyte <superkalel55@gmail.com>
2023-08-25 20:13:01 +0100

    Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -37,10 +37,38 @@ class TestGetJson(unittest.TestCase):
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
+    @unittest.mock.patch('requests.get')
     def test_get_json(self, test_url, test_payload):
         """ Test that utils.get_json returns the expected result """
-        self.assertEqual(utils.get_json(test_url), test_payload)
+        mock_response = unittest.mock_get.return_value
+        mock_response.json.return_value = test_payload
+
+        result = utils.get_json(test_url)
+        self.assertEqual(result, test_payload)
+        mock_response.get.assert_called_once_with()
 
 
 class TestMemoize(unittest.TestCase):
-    """ Class for testing memoize function """
+    """ Class for testing memoize decorator """
+
+    def test_memoize(self):
+        """ Test that when calling a_property twice, the correct result is
+        returned but a_method is only called once using assert_called_once
+        """
+        class TestClass:
+            """ TestClass that inherits from BaseClass """
+
+            def a_method(self):
+                """ Returns the attribute 'a' """
+                return 42
+
+            @utils.memoize
+            def a_property(self):
+                """ Returns memoized property """
+                return self.a_method()
+
+        with unittest.mock.patch.object(TestClass, 'a_method') as mock:
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+            mock.assert_called_once()
