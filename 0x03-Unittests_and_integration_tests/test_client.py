2089f7687c2754db183ac7bedbb883f7d5bee2d4
kalwhyte <superkalel55@gmail.com>
2023-08-28 21:57:51 +0100

    This is a test case class named TestGithubOrgClient that inherits from unittest.TestCase. It is used to test the behavior of the GithubOrgClient class. The @parameterized.expand decorator is used to parameterize the test_org method. It allows you to run the same test method with multiple sets of parameters. The parameters passed to @parameterized.expand are the names of organizations (e.g., google, abc) that you want to test. The @patch('client.get_json') decorator is used to patch the get_json function in the client module. This is done to ensure that the actual HTTP request is not made during the test. The test_org method is the actual test method that will be executed for each set of parameters. Inside the test_org method: An instance of the GithubOrgClient class is created with the organization name (test_org parameter). The org method of the GithubOrgClient instance is called. The mock_get_json.assert_called_once_with assertion verifies that the get_json function was called once with the expected URL, which is generated based on the test_org parameter. Overall, this test is checking whether the org method of the GithubOrgClient class correctly calls the get_json function with the appropriate URL for different organizations. The use of the @patch decorator ensures that no actual HTTP requests are made during the test, and the mocked behavior is used instead.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -9,31 +9,15 @@ from client import GithubOrgClient
 from utils import get_json, access_nested_map, memoize
 
 
-""" In a new test_client.py file, declare the TestGithubOrgClient(unittest.TestCase) class and implement the test_org method.
-
-This method should test that GithubOrgClient.org returns the correct value.
-
-Use @patch as a decorator to make sure get_json is called once with the expected argument but make sure it is not executed.
-
-Use @parameterized.expand as a decorator to parametrize the test with a couple of org examples to pass to GithubOrgClient, in this order:
-
-google
-abc
-Of course, no external HTTP calls should be made.
-"""
-
-
 class TestGithubOrgClient(unittest.TestCase):
-    """ Class for testing GithubOrgClient
-    """
+    """ Class for testing GithubOrgClient """
     @parameterized.expand([
         ("google"),
         ("abc"),
     ])
     @patch('client.get_json')
     def test_org(self, test_org, mock_get_json):
-        """ Test that GithubOrgClient.org returns the correct value
-        """
+        """ Test that GithubOrgClient.org returns the correct value """
         test_class = GithubOrgClient(test_org)
         test_class.org()
         mock_get_json.assert_called_once_with(
