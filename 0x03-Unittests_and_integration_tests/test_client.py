675e1e0054727f1fbd379c0aef636f95d2492ee8
kalwhyte <superkalel55@gmail.com>
2023-08-29 17:50:42 +0100

    test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -24,7 +24,8 @@ class TestGithubOrgClient(unittest.TestCase):
         ('abc', {'login': 'abc'}),
     ])
     @patch('client.get_json')
-    def test_org(self, test_org: str, resp: Dict, mocked_whyte: MagicMock) -> None:
+    def test_org(self, test_org: str, resp: Dict,
+                 mocked_whyte: MagicMock) -> None:
         """ Test GithubOrgClient.org method """
         mocked_whyte.return_value = MagicMock(return_value=resp)
         whyte_class = GithubOrgClient(test_org)
@@ -49,44 +50,35 @@ class TestGithubOrgClient(unittest.TestCase):
         result = test_class._public_repos_url()
 
         expected_url = 'https://api.github.com/orgs/testorg/repos'
+        result.assert_called_once_with()
         self.assertEqual(result, expected_url)
 
-    @patch('client.get_json')
-    def test_public_repos(self, mock_get: MagicMock) -> None:
+    @patch('client.get_json', return_value=[{'name': 'google'},
+                                            {'name': 'abc'}])
+    def test_public_repos(self, mock_get):
         """ Test GithubOrgClient.public_repos method """
-        mock_payload = [{"name": "google"}, {"name": "abc"}]
-        mock_get.return_value = mock_payload
-        with patch.object(
-            GithubOrgClient, '_public_repos_url',
-                new_callable=PropertyMock) as mock_public_repos_url:
-            mock_public_repos_url.return_value = 'www.whyte.com'
-            whyte_class = GithubOrgClient('test')
-            result = whyte_class.public_repos()
-            self.assertEqual(result, ['google', 'abc'])
-            mock_public_repos_url.assert_called_once_with()
-        mock_get.assert_called_once_with('www.whyte.com')
+        test_class = GithubOrgClient('test')
+        self.assertEqual(
+            test_class.public_repos(),
+            ['google', 'abc']
+        )
+        mock_get.assert_called_once_with(
+            'https://api.github.com/orgs/test/repos')
 
     @parameterized.expand([
         ({'license': {'key': 'my_license'}}, 'my_license', True),
         ({'license': {'key': 'other_license'}}, 'my_license', False),
     ])
     @patch('client.get_json')
-    def test_has_license(self, repo: Dict, license_key: str, expected: bool, mock_get: MagicMock) -> None:
+    def test_has_license(self, repo: Dict, license_key: str,
+                         expected: bool, mock_get: MagicMock) -> None:
         """ Test GithubOrgClient.has_license method """
-        mock_payload = {
-            'repos_url': 'https://api.github.com/repos/google/repos',
-            'repos': [repo]
-        }
-        mock_get.return_value = mock_payload['repos']
-        with patch.object(
-            GithubOrgClient, '_public_repos_url',
-                new_callable=PropertyMock) as mock_public_repos_url:
-            mock_public_repos_url.return_value = mock_payload['repos_url']
-            whyte_class = GithubOrgClient('test')
-            result = whyte_class.has_license(repo, license_key)
-            self.assertEqual(result, expected)
-            mock_public_repos_url.assert_called_once_with()
-        mock_get.assert_called_once_with(mock_payload['repos_url'])
+        mock_get.return_value = repo
+        test_class = GithubOrgClient('test')
+        self.assertEqual(
+            test_class.has_license(repo, license_key), expected)
+        mock_get.assert_called_once_with(
+            'https://api.github.com/orgs/test/repos')
 
 
 @parameterized_class([
